1546055105cf5c280fa08a3ef7efbdb7b19b4ba7
pepper-sister <yuyu7123@naver.com>
2025-11-03 05:14:35 +0900

    feat: Lotto클래스 사용 추가

--- src/print/printLottoNumber.js
+++ src/print/printLottoNumber.js
@@ -1,10 +1,11 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+import Lotto from "../Lotto.js";
 
 export default function printLottoNumber(lottoCount) {
   let count = lottoCount;
   while (count) {
-    const lottoArray = Random.pickUniqueNumbersInRange(1, 45, 6);
-    lottoArray.sort((a, b) => a - b);
+    const lottoArray = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+    new Lotto(lottoArray);
 
     Console.print(`[${lottoArray.join(", ")}]`);
     count--;
