e346bc1b2ed5b9626425be01cff7e6a5147aa5ea
pepper-sister <yuyu7123@naver.com>
2025-11-03 05:47:03 +0900

    feat: Lotto클래스 사용 추가

--- src/utils/getWinNumber.js
+++ src/utils/getWinNumber.js
@@ -1,9 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import { INPUT_WIN_NUMBER } from "../constants/message.js";
+import Lotto from "../Lotto.js";
 
 export default async function getWinNumber() {
   const winNumber = await Console.readLineAsync(INPUT_WIN_NUMBER);
   const withoutSpaceWinNumber = winNumber.replace(/\s/g, "");
-  const winNumberArray = withoutSpaceWinNumber.split(",");
+  const winNumberArray = withoutSpaceWinNumber.split(",").map(Number);
+  new Lotto(winNumberArray);
   return winNumberArray;
 }
