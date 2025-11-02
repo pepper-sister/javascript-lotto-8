254e48c0d4a984acc9b8b58b98b7b05f53f1ab56
pepper-sister <yuyu7123@naver.com>
2025-11-03 05:05:06 +0900

    feat: 발행된 로또 번호 오름차순 정렬

--- src/print/printLottoNumber.js
+++ src/print/printLottoNumber.js
@@ -4,6 +4,8 @@ export default function printLottoNumber(lottoCount) {
   let count = lottoCount;
   while (count) {
     const lottoArray = Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottoArray.sort((a, b) => a - b);
+
     Console.print(`[${lottoArray.join(", ")}]`);
     count--;
   }
