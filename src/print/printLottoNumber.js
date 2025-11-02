576c85e7391285a0ab352e51960726601ddc6b10
pepper-sister <yuyu7123@naver.com>
2025-11-03 05:46:45 +0900

    refactor: Lotto클래스 사용 제거

--- src/print/printLottoNumber.js
+++ src/print/printLottoNumber.js
@@ -1,15 +1,16 @@
 import { Random, Console } from "@woowacourse/mission-utils";
-import Lotto from "../Lotto.js";
 
 export default function printLottoNumber(lottoCount) {
+  const lottoArrays = [];
+
   let count = lottoCount;
   while (count) {
     const lottoArray = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
-    new Lotto(lottoArray);
 
     Console.print(`[${lottoArray.join(", ")}]`);
+    lottoArrays.push(lottoArray);
     count--;
   }
 
-  return;
+  return lottoArrays;
 }
